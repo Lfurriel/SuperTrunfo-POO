551fc70a328b3440173b657bcebd0b84afa57ce7
juliarmn <julia.rm.nascimento@unesp.br>
2023-06-04 12:50:32 -0300

    Tentativa de tela inicial

--- src/main/java/poo/trabalhofinal/supertrunfo/gui/controllers/TelaInicialController.java
+++ src/main/java/poo/trabalhofinal/supertrunfo/gui/controllers/TelaInicialController.java
@@ -1,26 +1,31 @@
 package poo.trabalhofinal.supertrunfo.gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class TelaInicialController {
+public class TelaInicialController implements Initializable {
 
     @FXML
-    private Label label;
+    private Button botao;
 
-    @FXML
-    private void handleMouseClick(MouseEvent event) {
-        System.out.println("Apertou (mouse)");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        botao.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("oioi");
+            }
+        });
     }
-
-    @FXML
-    private void handleKeyPress(KeyEvent event) {
-        System.out.println("Apertou (teclado)");
-    }
-
 }
