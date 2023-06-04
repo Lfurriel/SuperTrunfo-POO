a50c23982230cee341fb458a3e3460bb0ccc29e0
Lucas Furriel <lrodrigues@coachit.com.br>
2023-06-04 12:11:12 -0300

    Ajuste imagem

--- src/main/java/poo/trabalhofinal/supertrunfo/HelloApplication.java
+++ src/main/java/poo/trabalhofinal/supertrunfo/HelloApplication.java
@@ -4,6 +4,7 @@ package poo.trabalhofinal.supertrunfo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("telaInicial.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 610, 420);
-        stage.setFullScreen(true);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setResizable(false);
+//        stage.getIcons().add(new Image("https://img.freepik.com/vetores-premium/desenho-para-colorir-desenho-de-gato-lendo-livro-de-desenho-de-personagens_51194-266.jpg?w=2000"));
         stage.setTitle("SUPER TRUNFO");
         stage.setScene(scene);
         stage.show();
--- src/main/java/poo/trabalhofinal/supertrunfo/gui/controllers/TelaInicialController.java
+++ src/main/java/poo/trabalhofinal/supertrunfo/gui/controllers/TelaInicialController.java
@@ -1,17 +1,26 @@
 package poo.trabalhofinal.supertrunfo.gui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class TelaInicialController {
+
     @FXML
-    public void onKeyPressed(KeyEvent event) {
-        System.out.println("Apertou");
+    private Label label;
+
+    @FXML
+    private void handleMouseClick(MouseEvent event) {
+        System.out.println("Apertou (mouse)");
     }
 
     @FXML
-    public void onMouseClicked(MouseEvent event) {
-        System.out.println("Apertou");
+    private void handleKeyPress(KeyEvent event) {
+        System.out.println("Apertou (teclado)");
     }
+
 }
