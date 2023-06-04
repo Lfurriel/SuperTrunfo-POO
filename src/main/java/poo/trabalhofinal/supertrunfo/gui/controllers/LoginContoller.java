674f8bc33fc25548f98038d8f04cb06cec1d45f9
juliarmn <julia.rm.nascimento@unesp.br>
2023-06-04 20:34:46 -0300

    QUERO TERMINARR

--- src/main/java/poo/trabalhofinal/supertrunfo/gui/controllers/LoginContoller.java
+++ src/main/java/poo/trabalhofinal/supertrunfo/gui/controllers/LoginContoller.java
@@ -8,8 +8,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import poo.trabalhofinal.supertrunfo.classes.Jogador;
+import poo.trabalhofinal.supertrunfo.classes.Jogo;
+import poo.trabalhofinal.supertrunfo.classes.cartas.Gato;
+import poo.trabalhofinal.supertrunfo.classes.cartas.LinguagensProgramacao;
+import poo.trabalhofinal.supertrunfo.classes.cartas.Personagem;
+import poo.trabalhofinal.supertrunfo.classes.exceptions.UsuarioNaoEncontradoException;
+import poo.trabalhofinal.supertrunfo.classes.interfaces.JogadoresRepositoryImpl;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginContoller implements Initializable {
@@ -29,6 +37,7 @@ public class LoginContoller implements Initializable {
     public Label alerta1;
     @FXML
     public Label alerta2;
+    String tipo;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -38,6 +47,28 @@ public class LoginContoller implements Initializable {
                 if(nome1.getText().equals("") || senha1.getText().equals("")) {
                     alerta1.setText("Todos os campos devem ser preenchidos.");
                 } else {
+                    if(tipo.equals("Personagem")) {
+                        JogadoresRepositoryImpl<Personagem> jogador = new JogadoresRepositoryImpl<>();
+                        try {
+                            jogador.buscaJogador(nome1.getText(), senha1.getText());
+                        } catch (SQLException | UsuarioNaoEncontradoException e) {
+                            alerta1.setText(e.getMessage());
+                        }
+                    } else if (tipo.equals("Gato")) {
+                        JogadoresRepositoryImpl<Gato> jogador = new JogadoresRepositoryImpl<>();
+                        try {
+                            jogador.buscaJogador(nome1.getText(), senha1.getText());
+                        } catch (SQLException | UsuarioNaoEncontradoException e) {
+                            alerta1.setText(e.getMessage());
+                        }
+                    } else {
+                        JogadoresRepositoryImpl<LinguagensProgramacao> jogador = new JogadoresRepositoryImpl<>();
+                        try {
+                            jogador.buscaJogador(nome1.getText(), senha1.getText());
+                        } catch (SQLException | UsuarioNaoEncontradoException e) {
+                            alerta1.setText(e.getMessage());
+                        }
+                    }
 
                 }
             }
